c5504cee1ab37b2b771865e241b6bf462452dc4f
Derek Wheeler <frozenarrow73@gmail.com>
2023-02-17 11:34:44 -0700

    completed questions in sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -12,6 +12,12 @@
 // Testing & Integration
 // Planning
 
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +25,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is when the plan becomes more concrete and the basic requirements of the program are defined, such as what technologies the program will need to work with" 
+const design = "this is when suedocode and or flow charts are used to design the logic of the code before and a roadmap is built so that code can be written more effecintly and without errors"
+const implementation = "This is when the code for the program is written"
+const testingAndIntegration = "This is when the code is uploaded for use and is tested to make sure that it works in all use cases, and any bugs are fixed."
+const maintain = " This is the final step, developers review and repair code. they will update the code to contine to work with new as technogies uses in the stack of the code change overtime"
